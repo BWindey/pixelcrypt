d351d06c79adc0c1c68a8f37834d6e95ac6c72a4
Kaya-Sem <kayasemvc@gmail.com>
2024-03-02 21:41:33 +0100

    functional (?) decode

--- decode.py
+++ decode.py
@@ -4,14 +4,38 @@ from loadThemeFile import loadThemeFile
 
 def loadImage(path="image.png", key="themes.toml"):
     theme = loadThemeFile()
-
     image = Image.open(path)
 
-    print("Image format:", image.format)
-    print("Image size:", image.size)
-    print("Image mode:", image.mode)
+    # debug information
+    print("-" * 30)
+    print("format:", image.format)
+    print("size:", image.size)
+    print("mode:", image.mode)
+    print("-" * 30)
 
-    image.show()
+    width, _ = image.size
+
+    # Retrieve RGB values of pixels in the first row
+    pixels_first_row_rgb = [image.getpixel((x, 0)) for x in range(width)]
+
+    print(theme.items())
+    keys = []
+    for rgb_value in pixels_first_row_rgb:
+        for key, theme_rgb in theme.items():
+            # Check if the RGB values are within a certain tolerance
+            if all(abs(a - b) <= 5 for a, b in zip(rgb_value, theme_rgb)):
+                keys.append(chr(int(key)))
+                break
+        else:
+            keys.append(None)  # If no matching key is found, append None
+
+    # Filter out None values and concatenate the keys into a single string
+    keys_str = "".join(key for key in keys if key is not None)
+
+    print("-" * 50)
+    print(pixels_first_row_rgb)
+    print("-" * 50)
+    print(keys_str)
 
 
 loadImage(input())
