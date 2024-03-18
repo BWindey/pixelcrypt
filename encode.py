a787945e84672d37467fe143069dfb5555efbc5e
Kaya-Sem <kayasemvc@gmail.com>
2024-03-18 10:00:40 +0100

    updated printing zfill for consistency

--- encode.py
+++ encode.py
@@ -27,7 +27,7 @@ def generateImage(asciiValues: list, fileName: str, modus=str) -> None:
         # Extract RGB values from the list
         pixel_color = tuple(color)
         # print debug info
-        print(f"    {clearText[x]} ➡️ {ascii_val} ➡️ {pixel_color}")
+        print(f"    {clearText[x]} ➡️ {str(ascii_val).zfill(3)} ➡️ {pixel_color}")
 
         # Fill the entire column with the pixel color
         for y in range(size):
--- loadThemeFile.py
+++ loadThemeFile.py
@@ -1,17 +1,9 @@
 import toml
 
 
-# loads the theme file and returns its data
+# loads the theme file and returns it's data
 def loadThemeFile():
-    try:
-        with open("themes.toml", "r") as file:
-            data = toml.load(file)
-            theme = data.get("theme", {}).get("rainbow", None)
-            if theme is None:
-                raise ValueError("Theme 'rainbow' not found in the theme file.")
-            return theme
+    with open("themes.toml", "r") as file:
+        data = toml.load(file)
 
-    except FileNotFoundError:
-        print("Error: Theme file 'themes.toml' not found.")
-    except Exception as e:
-        print(f"An error occurred while loading the theme file: {e}")
+    return data["theme"]["rainbow"]
