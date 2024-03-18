e758a62bfc33a84524b1968ab9d93cbc07bc7c82
Kaya-Sem <kayasemvc@gmail.com>
2024-03-18 12:02:19 +0100

    added validity check before loading

--- loadThemeFile.py
+++ loadThemeFile.py
@@ -1,8 +1,11 @@
 import toml
 
+from validate_theme_toml import checker
+
 
 # loads the theme file and returns it's data
 def load_theme_file():
+    checker("themes.toml")  # validate encryption key
     with open("themes.toml", "r") as file:
         data = toml.load(file)
 
