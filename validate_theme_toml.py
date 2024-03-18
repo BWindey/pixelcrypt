6d29f37366da694adbbdb89819500e87bfd91be2
Kaya-Sem <kayasemvc@gmail.com>
2024-03-18 12:01:47 +0100

    made file available in other files -> validate theme on encode/decode

--- validate_theme_toml.py
+++ validate_theme_toml.py
@@ -17,13 +17,22 @@ def check_duplicate_values(theme_file):
     return duplicates
 
 
-if __name__ == "__main__":
-    theme_file = "themes.toml"
+def checker(theme_file: str):
     duplicate_rows = check_duplicate_values(theme_file)
 
     if duplicate_rows:
-        print("Duplicate rows found:")
+        print("Warning: duplicate rows found:")
         for theme_name, ascii_value in duplicate_rows:
             print(f"Theme: {theme_name}, ASCII Value: {ascii_value}")
+
+        print(
+            "\nproper encoding and decoding cannot be guaranteed with improper key theme file\n"
+        )
     else:
-        print("No duplicate rows found.")
+        print(
+            f"Encryption key {theme_file} loaded succesfully: no duplicate rows found.\n"
+        )
+
+
+if __name__ == "__main__":
+    checker("themes.toml")
