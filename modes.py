3938b40fa08b3ee97bbd91d6eaa48203add9be72
Kaya-Sem <kayasemvc@gmail.com>
2024-03-19 14:34:41 +0100

    added vertical bars mode

--- modes.py
+++ modes.py
@@ -1,6 +1,7 @@
 from abc import ABC
 
 
+# to be able to use an image mode, add it to the dictionary in encode
 class ImageMode(ABC):
     def apply(self, ascii_values: list, image, theme, size: int) -> None:
         pass
@@ -18,5 +19,20 @@ class DiagonalMode(ImageMode):
             # Fill first row with encoded message, then apply a shift for subsequent rows
             for y in range(size):
                 image.putpixel(((x + y) % size, y), pixel_color)
+
+        return image
+
+
+class VerticalMode(ImageMode):
+    def apply(self, ascii_values: list, image, theme, size: int) -> None:
+        for x, ascii_val in enumerate(ascii_values):
+            pixel_color = tuple(theme[str(ascii_val)])
+
+            # Ensure that the color is a list
+            if not isinstance(pixel_color, tuple):
+                raise ValueError("Invalid color format in theme file")
+            # Fill the entire column with the pixel color
+            for y in range(size):
+                image.putpixel((x, y), pixel_color)
 
         return image
